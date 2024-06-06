dbe09ba911afaba935938dab42c24317c277fa03
521yuajiaqi <3394818961@qq.com>
2024-06-06 08:21:45 +0800

    Add files via upload
    输入框组件封装,包括父子组件通信

---
+++ 输入框组件封装/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app">
+    <ComInput v-model="content"></ComInput>
+  </div>
+</template>
+
+<script>
+import ComInput from "./components/ComInput.vue";
+export default {
+  components: {
+    ComInput,
+  },
+  data() {
+    return {
+      content: "",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ 输入框组件封装/components/ComInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="com-input">
+    <span>输入框:</span>
+    <input placeholder="请输入内容" @change="handleInput" :value="value" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: String,
+  },
+  methods: {
+    handleInput(e) {
+      this.$emit("input", e.target.value);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
